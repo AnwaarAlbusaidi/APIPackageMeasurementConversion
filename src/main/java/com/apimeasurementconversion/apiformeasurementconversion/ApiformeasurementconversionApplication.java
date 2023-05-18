f4e2105c690bbd7f2b25c689d048eb980f08d4ae
AnwaarAlbusaidi <AnwaarAlbusaidi5@gmail.com>
2023-05-18 06:43:18 +0400

    first commit

---
+++ src/main/java/com/apimeasurementconversion/apiformeasurementconversion/ApiformeasurementconversionApplication.java
@@ -0,0 +1,13 @@
+package com.apimeasurementconversion.apiformeasurementconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiformeasurementconversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiformeasurementconversionApplication.class, args);
+	}
+
+}
